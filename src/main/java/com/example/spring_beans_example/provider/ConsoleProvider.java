7dccdf6284927a0b041ebaa1681e943b4cdc09ee
The Plaintext Engineer <akshay2104.singh@gmail.com>
2026-02-03 12:55:19 +0000

    Refactored Code

--- src/main/java/com/example/spring_beans_example/provider/ConsoleProvider.java
+++ src/main/java/com/example/spring_beans_example/provider/ConsoleProvider.java
@@ -2,9 +2,7 @@ package com.example.spring_beans_example.provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ConsoleProvider implements NotificationProvider {
     private final Logger log = LoggerFactory.getLogger(ConsoleProvider.class);
 
