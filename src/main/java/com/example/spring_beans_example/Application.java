41bf628d354182564bd739b41184b4b1a9ebd64f
The Plaintext Engineer <akshay2104.singh@gmail.com>
2026-02-03 13:06:02 +0000

    notification-service as component

--- src/main/java/com/example/spring_beans_example/Application.java
+++ src/main/java/com/example/spring_beans_example/Application.java
@@ -2,17 +2,13 @@ package com.example.spring_beans_example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.example.spring_beans_example.provider.NotificationProvider;
-import com.example.spring_beans_example.service.NotificationServiceImpl;
+import com.example.spring_beans_example.service.NotificationService;
 
 public class Application {
     public static void main(String[] args) {
         try (var applicationContext = new AnnotationConfigApplicationContext("com.example.spring_beans_example")) {
-            var provider = (NotificationProvider) applicationContext.getBean("consoleProvider");
-
-            var notificationService = new NotificationServiceImpl(provider);
+            var notificationService = (NotificationService) applicationContext.getBean("notificationServiceImpl");
             notificationService.notify("Hello, Sir! You got a new message.");
-
         }
     }
 }
--- src/main/java/com/example/spring_beans_example/service/NotificationServiceImpl.java
+++ src/main/java/com/example/spring_beans_example/service/NotificationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.spring_beans_example.service;
 
+import org.springframework.stereotype.Service;
+
 import com.example.spring_beans_example.provider.NotificationProvider;
 
+@Service
 public class NotificationServiceImpl implements NotificationService {
   private final NotificationProvider provider;
 
